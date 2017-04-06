2bc64c0c3af93e7f7696e0c1f27db4b950fadfdd
Tyler Ladd Wilhelm <tylerwilhelm@Ansleys-MacBook-Pro.local>
2017-04-05 18:20:55 -0600

    Greet new users and others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  //send message to new user
+  socket.emit('newMessage', {
+  	from: 'Admin',
+  	text: 'Welcome to the Appeal Chat',
+  	createdAt: new Date().getTime()
+  });
+
+  //send message to all other users (new join)
+ 	socket.broadcast.emit('newMessage', {
+ 		from: 'Admin',
+ 		text: 'New User Joined',
+ 		createdAt: new Date().getTime()
+ 	})
 
   socket.on('createMessage', (message) => {
   	console.log('createMessage', message);
@@ -22,6 +35,11 @@ io.on('connection', (socket) => {
   		text: message.text,
   		createdAt: new Date().getTime()
   	});
+  	// socket.broadcast.emit('newMessage', {
+  	// 	from: message.from,
+  	// 	text: message.text,
+  	// 	createdAt: new Date().getTime()
+  	// });
   });
 
   socket.on('disconnect', () => {
